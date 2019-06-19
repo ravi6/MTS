cf385c335901a5ba90b7ff5fafd1910783e13cfb
ravi saripalli <ravi.saripalli@dsto.defence.gov.au>
2019-06-19 11:37:44 +1000

    moved Rewards to robot class

--- robot/tree.js
+++ robot/tree.js
@@ -73,6 +73,7 @@ class tree {    // The Game Tree (MonteCarlo Tree Search)
         this.simActionSeq = getActionSeq (posSeq) ; // to be used by propagate call
         
         // Stores as many sequences as we need in pdf ... 
+        // We write over the array from top to bottom ... (not a rolling buffer!!!)
         this.pdf.seq[(this.Nsim + 1)%pdf.size] = this.simActionSeq ;
 
         this.Nsim = this.Nsim + 1   ;  // simulation count 
@@ -123,114 +124,5 @@ class tree {    // The Game Tree (MonteCarlo Tree Search)
      };
      
    } // end info
-
-// We can make the following functions as complex as we wish
-    // for now we keep it very simple
-
-   TeamReward (seq) {
-        // This is unweighted Rollout reward, given a robot and rollout seq.
-        //  I am not sure if we have to use this or the next function which 
-       //   uses Conditional expected values for  backpropagation
-       //   My intution tells me that we need to use the later.
-         let reward = 0 ;
-         for (let i=0; i < this.robots.length ; i++) {
-              let rb = this.robots[i] ;
-              if (rb.id != this.robot.id){
-                   let js = rb.pdf.choose();
-                   if (js.seq != "none") {
-                          let sum = 0 ;
-                          for (let j=0 ; j< js.seq.length ; j++) {
-                              sum = sum + getReward( js.seq[j] ) ;
-                          }
-                          reward = reward + sum ; // note no probability multiplier
-                   }
-              } else { // calling robot case
-                          let sum = 0 ;
-                          for (let j=0 ; j< q.length ; j++) {
-                              sum = sum + getReward(seq) ;
-                          }
-                          reward = reward + sum ; 
-                      }
-         } // end loop over all robots 
-
-        return (reward) ; 
-
-   }
-
-
-    CondExpTeamReward (seq) {
-           // Calculates global Reward based on all other Robots action sets
-          //  given  action seq of this robot
-         //     Sum (G(X|x_k)*p(i!=k)   i=1..N
-         let reward = 0 ;
-         let robots = this.robot.team.robots ;
-         for (let i=0; i < robots.length ; i++) {
-              let rb = robots[i] ;
-              if (rb.id != this.robot.id){
-                   let js = rb.pdf.choose();
-                   if (js.seq != "none") {
-                          let sum = 0 ;
-                          for (let j=0 ; j< js.seq.length ; j++) {
-                              sum = sum + getReward( js.seq[j] ) ;
-                          }
-                          reward = reward + sum * js.q ;
-                   }
-              } else { // calling robot case
-                          let sum = 0 ;
-                          for (let j=0 ; j< q.length ; j++) {
-                              sum = sum + getReward(seq) ;
-                          }
-                          reward = reward + sum ;  
-                      }
-         } // end loop over all robots 
-
-        return (reward) ; 
-    } // end team reward
-
-
-    ExpTeamReward () {
-           // Calculates Expected global Reward based on all Robots action sets
-          //     Sum (G(X)*q(i))   i=1..N
-         let reward = 0 ;
-         let robots = this.robot.team.robots ;
-         for (let i=0; i < robots.length ; i++) {
-              let rb = robots[i] ;
-                   let js = rb.pdf.choose();
-                   if (js.seq != "none") {
-                          let sum = 0 ;
-                          for (let j=0 ; j< js.seq.length ; j++) {
-                              sum = sum + getReward( js.seq[j] ) ;
-                          }
-                          reward = reward + sum * js.q ;
-                          } }
-         } // end loop over all robots 
-
-        return (reward) ; 
-    } // end team reward
-
-
-
-    getReward (pos) {// Calculate reward moving to pos for any robot
-    
-        let reward = false ; 
-        let tps = this.robot.team.tps ;
-        for (let i=0; i<tps.length; i++ ) {
-            if (pos.x == tps[i][0] && pos.y == tps[i][1]) {
-                reward = true ;
-                break ;
-            }
-        } 
-
-        if (reward) {return (100);}
-        else {return(0);}       
-    } // end getReward
-
-
-   updateQ () {
-
-      for (let i=0 ; i < this.pdf
-
-
-   }
 
 } // end tree
