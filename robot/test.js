c86f4869734b29f856b9a9c5671ffc548073f575
ravi saripalli <ravi.saripalli@dsto.defence.gov.au>
2019-10-07 18:01:37 +1100

    Working on Robot Worker implementation

--- robot/test.js
+++ robot/test.js
@@ -1,20 +1,12 @@
-var obj ;
+var cat  = new Worker("./cat.js");
+var dog  = new Worker("./dog.js");
+cat.onmessage = responder ;
+dog.onmessage = responder ;
 
-function Test () {
+function responder (msg) {
+ // Message handler for all robot workers
+    // 
 
- obj = new dcmts() ;
-
-  // Read in parameters from Main Tab
- 
-   obj.params.maxCount = parseInt($("#MaxIter").val());
-   obj.pdfTraces = parseInt($("#pdfTraces").val()) ;
-   obj.params.beta.min = parseFloat($("#betaMin").val());
-   obj.params.beta.max = parseFloat($("#betaMax").val());
-   obj.params.beta.anneal = parseFloat($("#anneal").val());
-   obj.params.alpha = parseFloat($("#alpha").val());
-   //console.log(obj.params);
-  
-    obj.simulate() ;
+    if m
 }
-
 
